06e428a2c75d64ef04f253dfba3a9d8230a95076
Henry Derbyshire <henry@htderbyshire.com>
2022-01-17 17:51:01 +0000

    created project structure, added planning comments relating to javascript code

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// Todo List Item needs a:
+// - title
+// - description
+// - due date
+// - priority
+// - completed check box
+// - red, orange or green for different priorities
+
+// Factory function for a todo list item
+
+// Project needs a:
+// - list of zero or more todo items
+
+// Factory function for a project
+
+// Views:
+// - sidebar view
+// - project view
+// - all task view
+// - today's tasks view
+// - this week's tasks view
+// - project's tasks view
+
+// Modals:
+// - edit todo item
+// - create new todo item
+// - create new project
+
+// Todo Controller:
+// - delete todo
+// - show edit modal
+// - expand details
+
+// Project Controller:
+// - delete project

4e543bca9e18c6842b4e8c7546523dbc45e8153b
Henry Derbyshire <henry@htderbyshire.com>
2022-01-18 19:45:02 +0000

    added comments on what javascript tasks need to be completed

--- src/index.js
+++ src/index.js
@@ -33,3 +33,25 @@
 
 // Project Controller:
 // - delete project
+
+//
+//
+//
+//
+// changing active button on the menu
+// adding project modal when new project is pressed
+// adding new todo task modal when add a new project is pressed
+// decide on how data for tasks is stored
+// submitting project modal adds a new project - how is it stored?
+// submitting todo task modal adds a new task - how is it stored?
+// add event listeners to tasks
+// show details slider when down arrow is pressed
+// change arrow icon when details are showing and when they are not
+// show edit slider when edit is pressed
+// showing task priority colour as the left border colour of the task
+// completing task - how does it interact with data
+// strike through the title when the task is complete
+// deleteing task - how does it interact with data
+// populate the today and this week views with the correct tasks
+// populate each project with the correct tasks
+//
